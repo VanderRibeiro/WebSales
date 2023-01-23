12cc5bbccbf87e5b067c7a1260cf36690c419c48
VanderRibeiro <vbruno383@gmail.com>
2023-01-22 22:21:46 -0300

    Creating Edit

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WebSales.Models.ViewModels;
 using WebSales.Models;
 using WebSales.Services;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Controllers
 {
@@ -76,7 +77,47 @@ namespace WebSales.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try 
+            { 
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DllNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
